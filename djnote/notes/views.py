28375a43778e454dbcc46d3afc9d32cb387de6a0
christianaveros <christianaveros@gmail.com>
2017-04-27 17:24:03 +0800

    improve version of  site

--- djnote/notes/views.py
+++ djnote/notes/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+from .models import Note
+
+class Index(View):
+	template_name = 'notes/index.html'
+
+	def get_context_data(self):
+		context = {
+			'notes' : Note.objects.order_by('-id')
+		}
+		return context
+
+	def get(self, request, *args, **kwargs):
+		context = self.get_context_data()
+		return render(request, self.template_name, context)
